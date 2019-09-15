51e34750f459afc5c6710ed9ee83c58e1652ecaa
Anirudh Pillutla <a.v.pillutla@gmail.com>
2019-09-14 23:06:37 -0400

    adding chain updates to chain-runner

--- chain-runner.py
+++ chain-runner.py
@@ -4,9 +4,9 @@ import requests
 import json
 
 nodes = {
-  'Anirudh': 'http://192.168.0.23:5000',
-  'Martin': 'http://192.168.0.9:5001'
-  # 'Tamarcus': 'http://192.168.0.29:5002'
+  # 'Anirudh': 'http://192.168.0.23:5000',
+  # 'Martin': 'http://192.168.0.9:5001'
+  'Tamarcus': 'http://192.168.0.29:5002'
 }
 
 chain1 = Blockchain()
@@ -32,10 +32,10 @@ def get_chain():
 @app.route('/addBlock')
 def add_block():
     chain1.add_new_block(chain1.hash(chain1.last_block))
-    # for node, address in nodes.items():
-    #     if (node==chain1.name):
-    #         continue
-    #     requests.get(address + '/chainUpdated')
+    for node, address in nodes.items():
+        if (node==chain1.name):
+            continue
+        requests.get(address + '/chainUpdated')
     response = {"Block Added" : chain1.last_block}
     return jsonify(response), 200
 
@@ -45,3 +45,16 @@ def add_transaction():
     chain1.add_transaction("Testing 123")
     response = "Added Transaction"
     return jsonify(response), 200
+
+@app.route('/resolveChain')
+def resolve_chain():
+    response = requests.get('http://192.168.0.23:5000/getChain').json()
+    foreign_chain = response["Full Chain"]
+    if len(foreign_chain) > len(chain1.chain):
+        chain1.chain = foreign_chain
+    return jsonify("Foreign Chain is Longer, Updating Local Chain..."), 200
+
+@app.route('/chainUpdated')
+def call_resolve_chain():
+    resolve_chain()
+    return jsonify("Received Chain Update Notification...Will Resolve"), 200
